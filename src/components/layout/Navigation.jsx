3dbca47a3673cd4585067dbe3823dc53bf40a20f
handi7 <handi7.co@gmail.com>
2023-02-28 08:52:38 +0700

    Animate navigation menu

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,9 +1,22 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useTransition, animated } from "@react-spring/web";
 import React, { useState } from "react";
 
 export default function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
+
+  const slide = useTransition(showMenu, {
+    from: { transform: "translateX(-100%)" },
+    enter: { transform: "translateX(0%)" },
+    leave: { transform: "translateX(-100%)" },
+  });
+
+  const fade = useTransition(showMenu, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
 
   return (
     <nav>
@@ -14,17 +27,30 @@ export default function Navigation() {
         <FontAwesomeIcon icon={faBars} />
       </span>
 
-      {showMenu && (
-        <div
-          className="fixed bg-black-t-50 top-0 left-0 w-full h-full z-50"
-          onClick={() => setShowMenu(!showMenu)}
-        />
+      {fade(
+        (style, show) =>
+          show && (
+            <animated.div
+              style={style}
+              className="fixed bg-black-t-50 top-0 left-0 w-full h-full z-50"
+              onClick={() => setShowMenu(!showMenu)}
+            />
+          )
       )}
 
-      {showMenu && (
-        <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3">
-          Menu
-        </div>
+      {slide(
+        (style, show) =>
+          show && (
+            <animated.div
+              style={style}
+              className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+            >
+              <div className="font-bold border-b py-3">Menu</div>
+              <ul>
+                <li>Home</li>
+              </ul>
+            </animated.div>
+          )
       )}
     </nav>
   );
