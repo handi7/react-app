e9b0842bc9a43bda6998c789580d1485edc2c9eb
handi7 <handi7.co@gmail.com>
2023-02-28 12:21:05 +0700

    Edit title

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="fixed w-full border-b shadow bg-white p-3 top-0 flex justify-between item-center">
       <Link to="/">
-        <span className="font-bold">AppName</span>
+        <span className="font-bold">Mini App</span>
       </Link>
 
       <Navigation />
